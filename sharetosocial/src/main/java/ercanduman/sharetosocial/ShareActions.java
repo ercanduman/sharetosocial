af09f7242d105aa5c7804130383f0892b4dbfbfb
Ercan Duman <ercanduman30@gmail.com>
2019-01-02 10:24:23 +0300

    module added to project

---
+++ sharetosocial/src/main/java/ercanduman/sharetosocial/ShareActions.java
@@ -0,0 +1,11 @@
+package ercanduman.sharetosocial;
+
+import android.util.Log;
+
+public class ShareActions {
+
+    public static void shareAppGeneral() {
+        Log.d("eduman", "shareAppGeneral: called");
+    }
+
+}
